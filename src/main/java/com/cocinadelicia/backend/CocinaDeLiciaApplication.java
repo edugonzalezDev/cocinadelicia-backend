86b06e4cb9088a36a6ca84bca82088545aef4a87
Eduardo <elduz.g@gmail.com>
2025-10-15 19:13:49 -0300

    chore: inicialización del proyecto Spring Boot

---
+++ src/main/java/com/cocinadelicia/backend/CocinaDeLiciaApplication.java
@@ -0,0 +1,13 @@
+package com.cocinadelicia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocinaDeLiciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocinaDeLiciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocinadelicia/backend/CocinaDeLiciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocinadelicia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocinaDeLiciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
